0ccca9f95478664bf002ea5fe139be5d2e74c7e9
Brett Dorfman <123748945+Bdorf04@users.noreply.github.com>
2023-09-19 18:42:16 -0400

    Added README, made bot more efficient

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Cookie_Clicker
+# Cookie_Clicker Bot
+## Made by Brett Dorfman
+
+This Cookie Clicker bot automatically progresses you in the game, up until the temple. The ability to continue playing once the bot has run its course is possible. More automatic progress will be added soon.  
+
+### How to Use this bot
+
+1. Downlaod Python [Here][https://www.python.org/downloads/]
+
+2. Downlaod Selenium [Here][https://pypi.org/project/selenium/]
+
+4. Download a webdriver. I use the chrome Webdriver, attached [Here][https://chromedriver.chromium.org/downloads] Keep in mind, some selenium versions and webdrivers force you to set up a path, but the newest selenium and webdriver versions do it automatically. 
+
+3. Clone this repository, and run the bot!
+
+Please keep in mind any progress done within the browser will be lost upon closing.
+
+---
+
+Any questions, suggestions, issues? Feel free to email me at bdorfman04@gmail.com.
+
+Another way to "hack" cookie clicker is to inspect element and run the command game.cookies={Number}
+
+Happy clicking!!
--- selenim4CookieClicker.py
+++ selenim4CookieClicker.py
@@ -10,97 +10,59 @@ driver.implicitly_wait(2)
 lang=driver.find_element(By.ID, "langSelect-EN")
 lang.click()
 
-driver.implicitly_wait(4)
+time.sleep(4)
 
 count=driver.find_element(By.ID, "cookies")
 cookie=driver.find_element(By.ID, "bigCookie")
 
-c1,c2,c3,c4,c5,c6,c7,c0,c8=0,0,0,0,0,0,0,0,0
-while True:
-    for i in range(1000):
-        cookie.click()
-    wealth=int(''.join([i for i in (count.text.split(" ")[0]) if i.isnumeric()]))
-    try:
-        price=driver.find_element(By.ID, "productPrice8")
-        if wealth>int(price.text) and c0<10:
-            c0+=1
-            upgrade=driver.find_element(By.ID, "product8")
-            upgrade.click()
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice7")
-        if wealth>int(price.text) and c1<10:
-            c1+=1
-            upgrade=driver.find_element(By.ID, "product7")
-            upgrade.click()
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice6")
-        price=int(''.join([i for i in price.text if i.isnumeric()]))
-        if wealth>int(price) and c2<10:
-            c2+=1
-            upgrade=driver.find_element(By.ID, "product6")
-            upgrade.click()
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice5")
-        price=int(''.join([i for i in price.text if i.isnumeric()]))
-        if wealth>int(price) and c3<10:
-            c3+=1
-            upgrade=driver.find_element(By.ID, "product5")
-            upgrade.click()
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice4")
-        price=int(''.join([i for i in price.text if i.isnumeric()]))
-        if wealth>int(price) and c4<10:
-            c4+=1
-            upgrade=driver.find_element(By.ID, "product4")
-            upgrade.click()
-            print("Factory Bought")
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice3")
-        price=int(''.join([i for i in price.text if i.isnumeric()]))
-        if wealth>int(price) and c5<10:
-            c5+=1
-            upgrade=driver.find_element(By.ID, "product3")
-            upgrade.click()
-            print("Mine Bought")
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice2")
-        price=int(''.join([i for i in price.text if i.isnumeric()]))
-        if wealth>int(price) and c6<10:
-            c6+=1
-            upgrade=driver.find_element(By.ID, "product2")
-            upgrade.click()
-            print("Farm Bought")
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice1")
-        price=int(''.join([i for i in price.text if i.isnumeric()]))
-        if wealth>int(price) and c7<10:
-            c7+=1
-            upgrade=driver.find_element(By.ID, "product1")
-            upgrade.click()
-            print("Grandma Bought")
-    except:
-        pass
-    try:
-        price=driver.find_element(By.ID, "productPrice0")
-        price=int(''.join([i for i in price.text if i.isnumeric()]))
-        if wealth>int(price) and c8<10:
-            c8+=1
-            upgrade=driver.find_element(By.ID, "product0")
-            upgrade.click()
-            print("Cursor Bought")
-    except:
-        pass
+
+for i in range(700):
+    cookie.click()
+
+upgrade=driver.find_element(By.ID, "product0")  #buying 10 Clickers
+for i in range(10):
+    upgrade.click()
+
+for i in range(10):
+    cookie.click()
+
+driver.find_element(By.CSS_SELECTOR, ".crate.upgrade.enabled").click()  #Buying first cursor upgrade
+
+for i in range(500):
+    cookie.click()
+
+upgrade=driver.find_element(By.ID, "product1")  #buying 10 grandmas
+for i in range(10):
+    upgrade.click()
+
+for i in range(500):
+    cookie.click()
+
+for i in range(2):
+    driver.find_element(By.CSS_SELECTOR, ".crate.upgrade.enabled").click()  #Buying 2 upgrades'
+    driver.implicitly_wait(2)
+
+for i in range(5000):
+    cookie.click()
+print('right behin the farms')
+upgrade=driver.find_element(By.ID, "product2")  #Buying 10 Farms
+for i in range(10):
+    upgrade.click()
+print("bought the farms")
+for i in range(5000):
+    cookie.click()
+
+driver.find_element(By.CSS_SELECTOR, ".crate.upgrade.enabled").click()  #Buying next 2 upgrades
+
+print("We made it here!!!")
+for i in range(5):
+    cookie.click(10000)
+    for i in range(10):
+        upgrade=driver.find_element(By.ID, f"product{i+3}")
+    cookie.click(10000)
+    for i in range(5):
+        driver.find_element(By.CSS_SELECTOR, ".crate.upgrade.enabled").click()
+        driver.implicitly_wait(1)
+
+print("Bot Ended. Simply Continue Playing!")
+driver.implicitly_wait(100000)
